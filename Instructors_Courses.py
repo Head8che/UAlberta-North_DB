c9e24f82c32a1888ce76bffc001efe89d64ccc78
Head8che <hagi@ualberta.ca>
2021-03-04 22:31:35 -0700

    minor changes to the data being captured

--- Instructors_Courses.py
+++ Instructors_Courses.py
@@ -6,12 +6,10 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import os
 
 class Course:
     # def __init__(self):
     pass
-
 def configure_driver():
     # Add additional Options to the webdriver
     chrome_options = Options()
@@ -21,25 +19,23 @@ def configure_driver():
     # For linux/Mac
     # driver = webdriver.Chrome(options = chrome_options)
     # For windows
-    driver = webdriver.Chrome("C:/Users/Zubier/Desktop/UAlberta-North/UAlberta/chromedriver.exe")
+    driver = webdriver.Chrome("C:/Users/Zubier/Desktop/UAlberta-North_DB/chromedriver.exe")
     return driver
 
 
 def getCourses(driver):
     uAlberta_courses = []
-    uAlberta_Northern_courses = []
-    for each_keyword in contentList:
-        # each_keyword = each_keyword[1].strip
+    for each_keyword in facultyList:
         each_keyword = f'"{each_keyword[1]}"'
         driver.get(f"https://www.ualberta.ca/search/index.html#q={each_keyword}&t=Courses&sort=relevancy")
         time.sleep(.5)
         # wait for the element to load
         try:
             WebDriverWait(driver, 2).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))
-           
         except TimeoutException:
-            print("TimeoutException: Element not found")
+            # print("TimeoutException: Element not found")
             continue
+            # return None
 
         # Step 2: Create a parse tree of page sources after searching
         soup = BeautifulSoup(driver.page_source, "lxml")
@@ -47,14 +43,13 @@ def getCourses(driver):
         result = numofPages[0].text
 
         for index in range(int(int(result)/10)+1):
-            print(index,(int(result)/10))
             driver.get(f"https://www.ualberta.ca/search/index.html#q={each_keyword}&first={index*10}&t=Courses&sort=relevancy")
             time.sleep(.5)
             # wait for the element to load
             try:
                 WebDriverWait(driver, 2).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))     
             except TimeoutException:
-                print("TimeoutException: Element not found")
+                # print("TimeoutException: Element not found")
                 return None
                     
             soup = BeautifulSoup(driver.page_source, "lxml")
@@ -62,7 +57,7 @@ def getCourses(driver):
             for course_page in soup.select("div.CoveoResult"):
                 for course in course_page.find_all('a'):
                     course_num1 = Course()
-                    course_num1.title = course.text
+                    # course_num1.title = course.text
                     course_num1.url = course.get('href')
                     uAlberta_courses.append(course_num1)
 
@@ -77,18 +72,26 @@ def getCourses(driver):
         course_description = soup.h2
         course_description = course_description.text.strip()
         course_description = f'"{course_description}"'
+        course_url = f'"{course.url}"'
+        
         for faculty_course_description_instructor in soup.select("div.content:nth-child(4)"):
             for every_faculty_course_description in faculty_course_description_instructor.find_all('div', attrs={'class': 'card mt-4 dv-card-flat'}):    
-                # print(every_faculty_course_description)
                 course_description_term = every_faculty_course_description.h4.string
                 for every_course_description_term in every_faculty_course_description.find_all('div',attrs={'class': 'col-lg-4 col-12 pb-3'}):
                     try:
                         course_description_type = f'"{every_course_description_term.strong.text.strip()}"'
                     except AttributeError:
                         course_description_type = f'"{"N/A"}"'
-                    course_description_time = '"'+every_course_description_term.em.text.strip().replace("\n", " ")+'"'
-                    # course_prof = every_course_description_term.find_all('a')[0]
-               
+                    # course_description_date = '"'+every_course_description_term.em.text.strip().split('\n')+'"'
+                    course_description_date = every_course_description_term.em.text.strip().split('\n')
+                    if len(course_description_date) > 1:
+                        course_description_time = '"'+course_description_date[1]+'"'
+                    else:
+                        course_description_time = f'"{"N/A"}"'
+                    course_description_date = '"'+course_description_date[0]+'"'
+
+                    
+
                     try:
                         course_prof = every_course_description_term.find_all('a')
                         if len(course_prof) == 2:
@@ -96,39 +99,36 @@ def getCourses(driver):
                         else:
                             course_prof = course_prof[0].text.strip()
                     except IndexError:
-                        # print("AttributeError:: Element not found")
                         course_prof = f'"{"N/A"}"'
-                    for each_instructor in contentList:
+                    for each_instructor in facultyList:
                         if course_prof == each_instructor[1]: 
-                            print(course_prof,each_instructor[1])
                             course_prof = f'"{course_prof}"'         
-                            final_string = course_description+','+course_description_term + "," + course_description_type + "," + course_description_time + "," + course_prof 
+                            final_string = course_description+',' + course_url + "," + course_description_term + "," + course_description_type + "," + course_description_date + "," + course_description_time + "," +  course_prof 
                             final_string1 = final_string + "\n"
                         elif course_prof != each_instructor[1]:
                             continue
                         else:
                             course_prof = f'"{course_prof}"' 
                             print(course_prof) 
-                            final_string = course_description+','+course_description_term + "," + course_description_type + "," + course_description_time + "," + course_prof 
+                            final_string = course_description+',' + course_url + "," +course_description_term + "," + course_description_type + "," + course_description_date + "," + course_description_time +  "," + course_prof 
                             final_string1 = final_string + "\n"
                     f.write(final_string1)
 
 # create the driver object.
 driver = configure_driver()
-with open("C:/Users/Zubier/Desktop/UAlberta-North/UAlberta/Faculty_Members.txt") as f:
+with open("./Faculty_Members.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content] 
-contentList = []
+facultyList = []
 for word in content:
     word = word.split(" - ")
-    contentList.append(word)
-for eachIndex in contentList:
-    # print(eachIndex[1].strip())
+    facultyList.append(word)
+for eachIndex in facultyList:
     search_faculty = eachIndex[0]
     search_keyword = eachIndex[1]
-# print(search_faculty,search_keyword,"TEST")
-# search_faculty = ["Faculty of Agricultural, Life and Environmental Sciences", "Faculty of Science"]
 getCourses(driver)
 # close the driver.
 driver.close()
+
+
--- main.py
+++ main.py
@@ -20,39 +20,43 @@ def configure_driver():
     # For linux/Mac
     # driver = webdriver.Chrome(options = chrome_options)
     # For windows
-    driver = webdriver.Chrome("C:/Users/Zubier/Desktop/UAlberta-North/UAlberta/chromedriver.exe")
+    driver = webdriver.Chrome("C:/Users/Zubier/Desktop/UAlberta-North_DB/chromedriver.exe")
     return driver
 
 def getCourses(driver):
     uAlberta_courses = []
-    uAlberta_Northern_courses = []
+    # special_keyword = [['-activity'], ['-Molecular Biology']]
     # search_faculty = ["Faculty of Agricultural, Life and Environmental Sciences", "Faculty of Science"]
-    # Step 1: Go to pluralsight.com, category section with selected search keyword.
-
     for each_keyword in search_keyword:
+        if type(each_keyword) == list:
+            each_keyword = f'"{each_keyword[0]}"' +  each_keyword[1]
+            each_keyword = str(each_keyword)
+        else:
+            each_keyword = f'"{each_keyword}"'
         driver.get(f"https://www.ualberta.ca/search/index.html#q={each_keyword}&t=Courses&sort=relevancy")
         time.sleep(.5)
         # wait for the element to load
         try:
-            WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))
-            
+            WebDriverWait(driver, 2).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))     
         except TimeoutException:
             print("TimeoutException: Element not found")
-            return None
+            continue
+            # return None
 
         # Step 2: Create a parse tree of page sources after searching
         soup = BeautifulSoup(driver.page_source, "lxml")
         numofPages = soup.select(".CoveoQuerySummary > span:nth-child(1) > span:nth-child(2)")
         result = numofPages[0].text
         # print(result)
+        # print(result)
 
         for index in range(int(int(result)/10)+1):
             driver.get(f"https://www.ualberta.ca/search/index.html#q={each_keyword}&first={index*10}&t=Courses&sort=relevancy")
             time.sleep(.5)
-            print(index,"/",int(int(result)/10)+1)
+            # print(index,"/",int(int(result)/10)+1)
             # wait for the element to load
             try:
-                WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))     
+                WebDriverWait(driver, 2).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.coveo-list-layout:nth-child(1)")))     
             except TimeoutException:
                 print("TimeoutException: Element not found")
                 return None
@@ -64,47 +68,72 @@ def getCourses(driver):
                     # print(course)
                     # for link in course.find_all('')
                     course_num1 = Course()
-                    course_num1.title = course.text
+                    # course_num1.title = course.text
                     course_num1.url = course.get('href')
                     uAlberta_courses.append(course_num1)
 
-    # print(uAlberta_courses)
     course = 0
     for course in uAlberta_courses:
         driver.get(course.url)
         time.sleep(.5)
+        f = open('test1.csv',"a")
         soup = BeautifulSoup(driver.page_source, "lxml")
         faculty_description = soup.select('div.pb-2:nth-child(3) > p:nth-child(3) > a:nth-child(1)')
         course.faculty = faculty_description[0].text.strip()
-        if course.faculty in search_faculty:
-            if len(uAlberta_Northern_courses) == 0:
-                uAlberta_Northern_courses.append(course)
-            else:
-                duplicate_found = False
-                for northernCourse in uAlberta_Northern_courses:
-                    if course.title == northernCourse.title:
-                        duplicate_found = True
-                        break
-                if duplicate_found == False:
-                    uAlberta_Northern_courses.append(course)
-            
-
-        course_description = soup.select('div.pb-2:nth-child(3) > p:nth-child(4)')
-        if len(course_description) == 0:
-                course.description = 'There is no available course description.'
-        else:
-                course.description = course_description[0].text.strip()
+        course_description = soup.h2
+        course_description = course_description.text.strip()
+        course_description = f'"{course_description}"'
+        course_url = f'"{course.url}"'
 
 
+        for faculty_course_description_instructor in soup.select("div.content:nth-child(4)"):
+            for every_faculty_course_description in faculty_course_description_instructor.find_all('div', attrs={'class': 'card mt-4 dv-card-flat'}):    
+                course_description_term = every_faculty_course_description.h4.string
+                for every_course_description_term in every_faculty_course_description.find_all('div',attrs={'class': 'col-lg-4 col-12 pb-3'}):
+                    try:
+                        course_description_type = f'"{every_course_description_term.strong.text.strip()}"'
+                    except AttributeError:
+                        course_description_type = f'"{"N/A"}"'
 
-    for val in uAlberta_Northern_courses:
-        print(val.title,val.faculty)
-        
+                    course_description_date = every_course_description_term.em.text.strip().split('\n')
+                    if len(course_description_date) > 1:
+                        course_description_time = '"'+course_description_date[1]+'"'
+                    else:
+                        course_description_time = f'"{"N/A"}"'
+                    course_description_date = '"'+course_description_date[0]+'"'
+                    
+
+
+
+                    try:
+                        course_prof = every_course_description_term.find_all('a')
+                        if len(course_prof) == 2:
+                            course_prof = course_prof[1].text.strip()
+                        else:
+                            course_prof = course_prof[0].text.strip()
+                    except IndexError:
+                        course_prof = f'"{"N/A"}"'
+
+
+
+                    for each_faculty in search_faculty:
+                        if each_faculty == course.faculty:
+                            course_faculty = f'"{course.faculty}"'
+                            final_string = course_description+','+ course_url + "," + course_faculty+ "," + course_description_term + "," + course_description_type + "," + course_description_date + "," + course_description_time + "," +  course_prof 
+                            final_content= final_string + "\n"
+                        elif each_faculty != course.faculty:
+                            continue
+                        else:
+                            course_faculty = f'"{course.faculty}"'
+                            final_string = course_description+','+ course_url + "," + course_faculty+ "," + course_description_term + "," + course_description_type + "," + course_description_date + "," + course_description_time + "," +  course_prof 
+                            final_content = final_string + "\n"
+                    f.write(final_content)
 
 # create the driver object.
 driver = configure_driver()
-search_keyword = ["Arctic", "circumpolar","Northern region","Canadian north","Canada's north","Northern courses","Northern landscape","northern systems","indigenous peoples"]
-search_faculty = ["Faculty of Agricultural, Life and Environmental Sciences", "Faculty of Science"]
+# search_keyword = ["Arctic"]
+search_keyword = ["Aboriginal", "Arctic", "Boreal","circumpolar","Northern regions","Canadian North","Canada's North"," Northern Regions","Northern landscapes","northern systems", ["indigenous peoples","-activity"], ["Northern", "-Molecular Biology"], 'climate and weather']
+search_faculty = ["Faculty of Agricultural, Life and Environmental Sciences", "Faculty of Science", "Faculty of Arts"," Faculty of Native Studies" , "Faculty of Public Health" , "Augustana Faculty" , "Faculty of Education" , "Faculty of Engineering" , "Faculty of Kinesiology, Sport, and Recreation" , "Faculty of Medicine & Dentistry"]
 getCourses(driver)
 # close the driver.
 driver.close()
